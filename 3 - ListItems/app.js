1cfc3f74055fd1f64d56eec1fabb3c5c270365ed
Damian Obajtek <damian.obajtek@gmail.com>
2020-01-19 19:12:27 +0100

    training with filter

--- 3 - ListItems/app.js
+++ 3 - ListItems/app.js
@@ -5,28 +5,46 @@ const data = {
       lastName: "Daniel",
       country: "United Stated",
       city: "Chicago",
-      avatar: "https://randomuser.me/api/portraits/men/81.jpg"
+      sex: "male"
     },
     {
       firstName: "Lauren",
       lastName: "Gilbert",
       country: "Australia",
       city: "Sydney",
-      avatar: "https://randomuser.me/api/portraits/women/44.jpg"
+      sex: "male"
+    },
+    {
+      firstName: "Teofila",
+      lastName: "Zenona",
+      country: "Polska",
+      city: "Sochaczew",
+      sex: "female"
+    },
+    {
+      firstName: "Agata",
+      lastName: "Madame",
+      country: "Polska",
+      city: "Kozy",
+      sex: "female"
     }
   ]
 };
 
 const Item = ({ user }) => (
-  <div>
-    <h1>Username: {user.firstName}</h1>
-    <h2>Lastname: {user.lastName}</h2>
+  <div className="userInfo">
+    <h1>{user.firstName}</h1>
+    <p>Info:</p>
+    <p>Lastname: {user.lastName}</p>
+    <p>Sex: {user.sex}</p>
   </div>
 );
 
 class ListItems extends React.Component {
   render() {
-    const users = this.props.data.users;
+    let users = this.props.data.users;
+    users = users.filter(user => user.sex === "female");
+
     const Items = users.map(user => <Item key={user.id} user={user} />);
 
     return <ul>{Items}</ul>;
